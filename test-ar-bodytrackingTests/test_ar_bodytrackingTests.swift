a5725ec0e96bff480347ac2fabebef461cd544a2
Daniel Mansour <demansou@gmail.com>
2021-12-12 15:01:47 -0600

    Initial Commit

---
+++ test-ar-bodytrackingTests/test_ar_bodytrackingTests.swift
@@ -0,0 +1,33 @@
+//
+//  test_ar_bodytrackingTests.swift
+//  test-ar-bodytrackingTests
+//
+//  Created by Daniel Mansour on 12/12/21.
+//
+
+import XCTest
+@testable import test_ar_bodytracking
+
+class test_ar_bodytrackingTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ test-ar-bodytrackingUITests/test_ar_bodytrackingUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  test_ar_bodytrackingUITestsLaunchTests.swift
+//  test-ar-bodytrackingUITests
+//
+//  Created by Daniel Mansour on 12/12/21.
+//
+
+import XCTest
+
+class test_ar_bodytrackingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
